7226e5fc1dcb7f45b414f914fac4b4682c83799e
Warren Velasco <warrenv@futuredigital360.com>
2025-04-05 03:09:45 +0800

    Integrate cms in terms and condition page

---
+++ src/lib/sanity.js
@@ -0,0 +1,8 @@
+import { createClient } from '@sanity/client'
+
+export const client = createClient({
+  projectId: import.meta.env.VITE_SANITY_PROJECT_ID,
+  dataset: import.meta.env.VITE_SANITY_DATASET,
+  apiVersion: '2023-05-03',
+  useCdn: true,
+}) 
